2d1a9bf042359b2357867bd7f93a22eea0102f21
AmanatsuToko <88881947+AmanatsuTouko@users.noreply.github.com>
2024-08-10 15:08:52 +0900

    クイズ画像を表示するImageの紐付け．

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,22 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // UDP通信
+    [Header("UDP Connection")]
     [SerializeField] UdpSender udpSender;
     [SerializeField] UdpReceiver udpReceiver;
     [SerializeField] DisplayLocalIP displayLocalIP;
 
     // 1階もしくは2階のデータ
+    [Header("Quiz Image Corresponding to NFC ID")]
     [SerializeField] ClientScriptableObject clientScriptableObject;
+
+    // クイズ画像表示用UI
+    [Header("UI")]
+    [SerializeField] Image QuizDisplayImage;
 
     void Start()
     {
@@ -40,5 +47,12 @@ public class GameManager : MonoBehaviour
 
         // cardIDに応じた処理を行う
         clientScriptableObject.DisplayQuestionImage(cardID);
-    }    
+    }
+
+    // NFCカードの識別番号と対応するクイズ画像のデータを更新する
+    void UpdateClientScriptableObject(ClientScriptableObject clientScriptableObject)
+    {
+        this.clientScriptableObject = clientScriptableObject;
+        clientScriptableObject.image = QuizDisplayImage;
+    }
 }
