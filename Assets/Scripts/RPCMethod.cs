b712de43508b334cd18c24b93889a47816baed43
AmanatsuToko <88881947+AmanatsuTouko@users.noreply.github.com>
2024-09-03 00:44:08 +0900

    UDPで関数名と引数を文字列として送受信できるようにした．
    InvokerクラスのHelper的な立ち位置
    関数名と可変長引数を文字列(json)にするシリアライザーとデシリアライザーを作成

---
+++ Assets/Scripts/RPCMethod.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// UDPで受信した文字列をもとに関数を実行できるようにする
+public class RPCMethod : MonoBehaviour
+{
+    // サンプルコード
+
+    // 実行する関数は，staticでpublicである必要がある
+    // public static void SampleMethod(int a, float b)
+    // {
+    //     Debug.Log((a, b));
+    // }
+
+    // void Start()
+    // {
+    //     MethodArgs methodArgs = new MethodArgs()
+    //     {
+    //         NameSpaceAndClassName = GetType().Name,
+    //         MethodName = nameof(SampleMethod),
+    //         Args = new List<string>() {"1", "23.4"}
+    //     };
+    //     string json = JsonUtility.ToJson(methodArgs);
+        
+    //     DoMethodFromJson(json);
+    // }
+
+    // 実行する関数の名前と引数のデータ
+    [System.Serializable]
+    public class MethodArgs
+    {
+        [SerializeField]
+        public string NameSpaceAndClassName;
+        [SerializeField]
+        public string MethodName;
+        [SerializeField]
+        public string[] Args;
+    }
+
+    public static void DoMethodFromJson(string jsonMethodArgs)
+    {
+        // Jsonをデシリアライズする
+        MethodArgs methodArgs = JsonUtility.FromJson<MethodArgs>(jsonMethodArgs);
+        // 関数を実行する（staticでpublicな関数限定）
+        Invoker.Invoke(methodArgs.NameSpaceAndClassName, methodArgs.MethodName, methodArgs.Args);
+    }
+
+    public static string GetJsonFromMethodArgs(string nameSpaceAndClassName, string methodName, string[] args)
+    {
+        MethodArgs methodArgs = new MethodArgs()
+        {
+            NameSpaceAndClassName = nameSpaceAndClassName,
+            MethodName = methodName,
+            Args = args
+        };
+        return JsonUtility.ToJson(methodArgs);
+    }
+}
